30002267460023f6b83af5a7426850a0cb09f6c1
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-02 13:47:07 +0200

    fix: update scss anim on history hov & vite config for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // base: '/namewiz-fe/',
+  base: "/fledesma-dev/",
   plugins: [react(), tsconfigPaths()],
   server: {
     host: true,
-    port: 5174
-  }
+    port: 5174,
+  },
 });
