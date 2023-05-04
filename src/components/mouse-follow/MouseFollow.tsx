47194377e422e4ad4c3bffef3200f8329e1eeeab
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-04 17:34:15 +0200

    feat: add point follow & hide bg foll

--- src/components/mouse-follow/MouseFollow.tsx
+++ src/components/mouse-follow/MouseFollow.tsx
@@ -1,24 +1,45 @@
-import { useRef, useEffect } from "react";
-import useCursorPosition from "@components/hooks/useCursorPosition";
-import "./mouse-follow.scss";
+import { useRef, useEffect } from 'react';
+import { useCursorPosition } from '@components/hooks/useCursorPosition';
+import './mouse-follow.scss';
 
 const MouseFollow = (): JSX.Element => {
-  const ref = useRef<HTMLDivElement>(null);
+  const refBackground = useRef<HTMLDivElement>(null);
+  const refPoint = useRef<HTMLDivElement>(null);
   const { x, y } = useCursorPosition();
 
   useEffect(() => {
-    if (ref.current) {
-      ref.current.animate(
+    if (refBackground.current) {
+      refBackground.current.animate(
         {
           left: `${x}px`,
-          top: `${y}px`,
+          top: `${y}px`
         },
-        { duration: 3000, fill: "forwards" }
+        { duration: 3000, fill: 'forwards' }
+      );
+    }
+    if (refPoint.current) {
+      refPoint.current.animate(
+        {
+          left: `${x}px`,
+          top: `${y}px`
+        },
+        { duration: 3000, fill: 'forwards' }
       );
     }
   }, [x, y]);
 
-  return <div ref={ref} className="mouse-follow"></div>;
+  return (
+    <>
+      {/* <div
+        ref={refBackground}
+        className="mouse-follow--bg"
+      ></div> */}
+      <div
+        ref={refPoint}
+        className="mouse-follow--point"
+      ></div>
+    </>
+  );
 };
 
 export default MouseFollow;
