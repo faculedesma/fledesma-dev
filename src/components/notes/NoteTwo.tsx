3f005ddb5c4c582ec5158439f7e93db0718c0ef8
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-16 23:08:31 +0200

    ref: move dice to note two

--- src/components/notes/NoteTwo.tsx
+++ src/components/notes/NoteTwo.tsx
@@ -1,21 +1,61 @@
 import { useEffect, useRef } from 'react';
 import { useIntersection } from '@components/hooks/useIntersection';
+import { useCursorPosition } from '@components/hooks/useCursorPosition';
+import DiceOne from '@assets/images/dice-one.png';
+import DiceTwo from '@assets/images/dice-two.png';
+import DiceFour from '@assets/images/dice-four.png';
 import './notes.scss';
 
 const NoteTwo = () => {
   const isMobile =
     window.innerWidth > 320 && window.innerWidth < 480;
   const noteTwoRef = useRef<HTMLDivElement>(null);
+  const diceOneRef = useRef<HTMLImageElement>(null);
+  const diceTwoRef = useRef<HTMLImageElement>(null);
+  const diceFourRef = useRef<HTMLImageElement>(null);
+
   const isInViewport = useIntersection(
     noteTwoRef,
     isMobile ? -100 : -300
   );
+  const { x, y } = useCursorPosition();
 
   useEffect(() => {
     if (isInViewport) {
       noteTwoRef.current?.classList.add('show-note');
     }
   }, [isInViewport]);
+
+  useEffect(() => {
+    if (
+      diceOneRef.current &&
+      diceTwoRef.current &&
+      diceFourRef.current &&
+      isInViewport
+    ) {
+      diceOneRef.current.animate(
+        {
+          left: `${x / 10}px`,
+          top: `-${y / 10}px`
+        },
+        { duration: 1618 * 2, fill: 'forwards' }
+      );
+      diceTwoRef.current.animate(
+        {
+          left: `${x / 20}px`,
+          top: `${y / 20}px`
+        },
+        { duration: 1618 / 2, fill: 'forwards' }
+      );
+      diceFourRef.current.animate(
+        {
+          left: `${x / 30}px`,
+          top: `-${y / 30}px`
+        },
+        { duration: 1618 * 2, fill: 'forwards' }
+      );
+    }
+  }, [x, y]);
 
   return (
     <div className="container">
@@ -24,6 +64,29 @@ const NoteTwo = () => {
           Investing time in fundamentals is essential, build
           a strong base and enjoy the ride.
         </h3>
+        <div className="dices">
+          <img
+            ref={diceTwoRef}
+            src={DiceTwo}
+            alt="dice-two"
+            draggable="false"
+            loading="lazy"
+          />
+          <img
+            ref={diceFourRef}
+            src={DiceFour}
+            alt="dice-four"
+            draggable="false"
+            loading="lazy"
+          />
+          <img
+            ref={diceOneRef}
+            src={DiceOne}
+            alt="dice-one"
+            draggable="false"
+            loading="lazy"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/services/Technologies.tsx
+++ src/components/services/Technologies.tsx
@@ -8,10 +8,6 @@ import { NodeJS } from '@assets/svgs/techs/NodeJS';
 import { Git } from '@assets/svgs/techs/Git';
 import { Figma } from '@assets/svgs/techs/Figma';
 import { useIntersection } from '@components/hooks/useIntersection';
-import { useCursorPosition } from '@components/hooks/useCursorPosition';
-import DiceOne from '@assets/images/dice-one.png';
-import DiceTwo from '@assets/images/dice-two.png';
-import DiceFour from '@assets/images/dice-four.png';
 
 interface ITechnologiesProps {
   isMobile: boolean;
@@ -21,14 +17,10 @@ export const Technologies: React.FC<ITechnologiesProps> = ({
   isMobile
 }) => {
   const technologiesRef = useRef<HTMLDivElement>(null);
-  const diceOneRef = useRef<HTMLImageElement>(null);
-  const diceTwoRef = useRef<HTMLImageElement>(null);
-  const diceFourRef = useRef<HTMLImageElement>(null);
 
-  const { x, y } = useCursorPosition();
   const isInViewport = useIntersection(
-    diceOneRef,
-    isMobile ? -75 : -150
+    technologiesRef,
+    isMobile ? -50 : -100
   );
 
   useEffect(() => {
@@ -38,37 +30,6 @@ export const Technologies: React.FC<ITechnologiesProps> = ({
       );
     }
   }, [isInViewport]);
-
-  useEffect(() => {
-    if (
-      diceOneRef.current &&
-      diceTwoRef.current &&
-      diceFourRef.current &&
-      isInViewport
-    ) {
-      diceOneRef.current.animate(
-        {
-          left: `${x / 10}px`,
-          top: `${y / 10}px`
-        },
-        { duration: 1618 * 2, fill: 'forwards' }
-      );
-      diceTwoRef.current.animate(
-        {
-          left: `${x / 20}px`,
-          top: `-${y / 20}px`
-        },
-        { duration: 1618 / 2, fill: 'forwards' }
-      );
-      diceFourRef.current.animate(
-        {
-          left: `-${x / 30}px`,
-          top: `${y / 30}px`
-        },
-        { duration: 1618 * 2, fill: 'forwards' }
-      );
-    }
-  }, [x, y]);
 
   return (
     <div ref={technologiesRef} className="technologies">
@@ -80,29 +41,6 @@ export const Technologies: React.FC<ITechnologiesProps> = ({
       <NodeJS />
       <Git />
       <Figma />
-      <div className="dices">
-        <img
-          ref={diceOneRef}
-          src={DiceOne}
-          alt="dice-one"
-          draggable="false"
-          loading="lazy"
-        />
-        <img
-          ref={diceTwoRef}
-          src={DiceTwo}
-          alt="dice-two"
-          draggable="false"
-          loading="lazy"
-        />
-        <img
-          ref={diceFourRef}
-          src={DiceFour}
-          alt="dice-four"
-          draggable="false"
-          loading="lazy"
-        />
-      </div>
     </div>
   );
 };
