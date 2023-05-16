d419cc6b3089fd368b639d46c63e5e7d39dd5374
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-16 18:47:26 +0200

    fix: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  base: '/fledesma-dev/',
-  plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 5174
-  }
+  base: '/',
+  plugins: [react(), tsconfigPaths()]
 });
