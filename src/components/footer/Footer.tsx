97ef1d7c8f4acbfb263a97483aaa24d6d2779e10
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-17 00:04:37 +0200

    fix: update footer bg

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -43,8 +43,6 @@ const Footer = () => {
       const ySetted = Math.round(
         (clientY / window.innerHeight) * 100
       );
-      console.log(xSetted, ySetted);
-      // matrixRef.current.style.clipPath = `circle(100px at ${xSetted}% ${ySetted}%)`;
       matrixRef.current.animate(
         {
           clipPath: `circle(100px at ${xSetted}% ${ySetted}%)`
@@ -57,12 +55,10 @@ const Footer = () => {
   const handleFollowMouse = () => {
     const mouse = document.getElementById('mouse-follow');
     mouse?.classList.remove('point');
-    mouse?.classList.add('matrix');
   };
 
   const handleLeaveMouse = () => {
     const mouse = document.getElementById('mouse-follow');
-    mouse?.classList.remove('matrix');
     mouse?.classList.add('point');
   };
 
@@ -70,7 +66,7 @@ const Footer = () => {
     if (matrixRef.current) {
       matrixRef.current.animate(
         {
-          clipPath: `circle(2000px at 0 0)`
+          clipPath: `circle(5000px at 0 0)`
         },
         { duration: 1618 * 2, fill: 'forwards' }
       );
@@ -96,6 +92,11 @@ const Footer = () => {
     >
       <div ref={footerRef} id="contact" className="footer">
         <h1>Let's build something awesome!</h1>
+        <div
+          className="footer-expand"
+          onMouseEnter={handleExpandImage}
+          onMouseLeave={handleCollapseImage}
+        ></div>
         <div className="footer-mail">
           <p>faculedesma1993@gmail.com</p>
           <div className="footer-mail--icon">
@@ -108,11 +109,6 @@ const Footer = () => {
             <p>{text}</p>
           </div>
         </div>
-        <div
-          className="footer-expand"
-          onMouseEnter={handleExpandImage}
-          onMouseLeave={handleCollapseImage}
-        ></div>
         <div className="footer-social">
           <Social />
         </div>
@@ -121,12 +117,14 @@ const Footer = () => {
             2023 <b>© Facundo Ledesma</b>
           </p>
         </div>
-        <img
-          ref={matrixRef}
-          src={MatirxPNG}
-          alt="matrix"
-          loading="lazy"
-        />
+        <div className="footer-bg">
+          <img
+            ref={matrixRef}
+            src={MatirxPNG}
+            alt="matrix"
+            loading="lazy"
+          />
+        </div>
       </div>
     </div>
   );
