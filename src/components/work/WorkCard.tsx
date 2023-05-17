78ff7a915a2c11e714db8a834270512b6ed633e9
Facundo Ledesma <facundo.ledesma@Facundos-MacBook-Pro.local>
2023-05-17 15:17:36 +0200

    fix: fix mouse follow on hover work card

--- src/components/work/WorkCard.tsx
+++ src/components/work/WorkCard.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from 'react';
 import { useIntersection } from '@components/hooks/useIntersection';
-import { useCursorFollowing } from '@components/hooks/useCursorFollowing';
-import { useCursorPosition } from '@components/hooks/useCursorPosition';
 
 interface IWork {
   id: string;
@@ -24,29 +22,12 @@ const WorkCard: React.FC<IWorkCardProps> = ({ work }) => {
     workCardRef,
     isMobile ? 0 : -250
   );
-  const isFollowing = useCursorFollowing({
-    targetRef: workCardRef
-  });
+
   useEffect(() => {
     if (isInViewport) {
       workCardRef.current?.classList.add('show-work-card');
     }
   }, [isInViewport]);
-
-  useEffect(() => {
-    const mouse = document.getElementById('mouse-follow');
-    if (isFollowing) {
-      mouse?.classList.add('discover');
-      mouse?.classList.add('show-discover');
-      mouse?.classList.remove('point');
-      mouse?.classList.remove('show-point');
-    } else {
-      mouse?.classList.add('point');
-      mouse?.classList.add('show-point');
-      mouse?.classList.remove('discover');
-      mouse?.classList.remove('show-discover');
-    }
-  }, [isFollowing]);
 
   const handleCardClick = () =>
     window.open(work.link, '_blank');
